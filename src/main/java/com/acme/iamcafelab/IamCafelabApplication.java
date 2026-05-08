3f6ca79bb11eb32aaa2d7d44662b0cf9efd1e286
Adriatic05 <175574196+adriandonayredev@users.noreply.github.com>
2026-05-07 19:13:28 -0500

    chore: initialize IAM microservice

---
+++ src/main/java/com/acme/iamcafelab/IamCafelabApplication.java
@@ -0,0 +1,13 @@
+package com.acme.iamcafelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamCafelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamCafelabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/iamcafelab/IamCafelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.iamcafelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamCafelabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
